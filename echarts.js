e96e45d211bbf7fbc0bea033d4964ca680024469
sunkuangdong <sunkuangdong@edianzu.com>
2022-02-22 23:47:49 +0800

    webpack 搭建未完成

---
+++ echarts.js
@@ -0,0 +1,41 @@
+var myChart = echarts.init(document.getElementById('main'))
+
+option = {
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        top: '5%',
+        left: 'center'
+    },
+    series: [
+        {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+                borderRadius: 10,
+                borderColor: '#fff',
+                borderWidth: 2
+            },
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '40',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [{value: 1048, name: 'Search Engine'}]
+        }
+    ]
+};
+
+option && myChart.setOption(option);
